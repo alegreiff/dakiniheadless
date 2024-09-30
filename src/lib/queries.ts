34fb8e212977a9a6497ddd58fb23d655c552abc8
Jaime de Greiff <alegreiff@gmail.com>
2024-09-30 12:04:10 -0500

    Headless Dakini

---
+++ src/lib/queries.ts
@@ -0,0 +1,22 @@
+import { API_BASE_URL } from "./constantes";
+
+export const getDatosBasicos = async() => {
+  const response = await fetch(API_BASE_URL,
+    {
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        query: `#graphql
+          query NewQuery {
+					settings: allSettings {
+						title: generalSettingsTitle
+						describe: generalSettingsDescription
+					}
+      }
+        `
+      }),
+    }); 
+
+    const { data } = await response.json();
+    return data;
+}
